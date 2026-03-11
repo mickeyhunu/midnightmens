c5d55513ec2b993a41e9a18cd71a289e148c2441
mickeyhunu <110079472+mickeyhunu@users.noreply.github.com>
2026-03-11 17:54:06 +0900

    feat(frontend): migrate static html pages into vue routes

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,13 +1,5 @@
-import AppNav from './components/AppNav.js';
-
 export default {
-  components: { AppNav },
   template: `
-    <div>
-      <AppNav />
-      <main class="container">
-        <router-view />
-      </main>
-    </div>
+    <router-view />
   `
 };
--- src/frontend/src/router/index.js
+++ src/frontend/src/router/index.js
@@ -1,19 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.js';
-import LoginView from '../views/LoginView.js';
-import RegisterView from '../views/RegisterView.js';
-import CreatePostView from '../views/CreatePostView.js';
-import PostDetailView from '../views/PostDetailView.js';
+import LegacyHtmlView from '../views/LegacyHtmlView.js';
+
+const routes = [
+  { path: '/', component: LegacyHtmlView, props: { page: 'index' } },
+  { path: '/login', component: LegacyHtmlView, props: { page: 'login' } },
+  { path: '/register', component: LegacyHtmlView, props: { page: 'register' } },
+  { path: '/create', component: LegacyHtmlView, props: { page: 'create-post' } },
+  { path: '/post-detail', component: LegacyHtmlView, props: { page: 'post-detail' } },
+  { path: '/bookmarks', component: LegacyHtmlView, props: { page: 'bookmarks' } },
+  { path: '/community', component: LegacyHtmlView, props: { page: 'community' } },
+  { path: '/my-page', component: LegacyHtmlView, props: { page: 'my-page' } },
+  { path: '/edit-post', component: LegacyHtmlView, props: { page: 'edit-post' } },
+  { path: '/admin', component: LegacyHtmlView, props: { page: 'admin' } },
+  { path: '/find-account', component: LegacyHtmlView, props: { page: 'find-account' } },
+  { path: '/business-info', component: LegacyHtmlView, props: { page: 'business-info' } },
+  { path: '/live', component: LegacyHtmlView, props: { page: 'live' } }
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [
-    { path: '/', component: HomeView },
-    { path: '/login', component: LoginView },
-    { path: '/register', component: RegisterView },
-    { path: '/create', component: CreatePostView },
-    { path: '/posts/:id', component: PostDetailView }
-  ]
+  routes
 });
 
 export default router;
